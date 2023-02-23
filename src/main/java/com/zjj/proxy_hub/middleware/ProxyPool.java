f60fc3e4619afe0f4266b059892100befc3131f3
zoujunjie <1277874614@qq.com>
2023-02-24 00:45:26 +0800

    ci

--- src/main/java/com/zjj/proxy_hub/middleware/ProxyPool.java
+++ src/main/java/com/zjj/proxy_hub/middleware/ProxyPool.java
@@ -1,17 +1,23 @@
 package com.zjj.proxy_hub.middleware;
 
 import com.zjj.proxy_hub.model.ProxyIp;
+import com.zjj.proxy_hub.scheduler.SpiderScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class ProxyPool {
 
     private final Set<ProxyIp> proxyPool = ConcurrentHashMap.newKeySet();
+
+    @Autowired
+    private SpiderScheduler spiderScheduler;
 
     public int size() {
         return proxyPool.size();
@@ -23,7 +29,16 @@ public class ProxyPool {
 
     public synchronized ProxyIp popProxy() {
         if (proxyPool.isEmpty()) {
-            return null;
+            try {
+                spiderScheduler.ScheduleResolve();
+                TimeUnit.MILLISECONDS.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return null;
+            }
+            if (proxyPool.isEmpty()) {
+                return null;
+            }
         }
         ProxyIp ip = proxyPool.iterator().next();
         proxyPool.remove(ip);
